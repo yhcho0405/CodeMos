00fca6aadb1da7c9cf747273d700ad666aef6ee5
Hoyeon <86115801+Hoyeon9@users.noreply.github.com>
2023-11-26 22:45:05 +0900

    Change request function request function is updated to work in asynchronized way properly add some skeletons to add actual code later

--- leader.js
+++ leader.js
@@ -1,6 +1,14 @@
 const serverAddress = "http://127.0.0.1:3000"
 
 var page = 1
+var saved
+
+const urls = [
+    "/api/ranking",
+    //페이지 증가/감소용 설정
+    //검색용 url
+]
+
 var getData
 
 var board = document.getElementById("board");
@@ -16,33 +24,49 @@ table.appendChild(tbody)
 
 thead.innerHTML = "<tr><td>Rank</td><td>Name</td><td>Score</td><td>Time</td></tr>"
 
-function getBoard(){
-    var ret = fetch(serverAddress + "/api/ranking",{
-        method: "GET",
-        body: null
+function getBoard(url){
+    return new Promise((resolve, reject) => {
+        fetch(serverAddress + url,{
+            method: "GET",
+            body: null
+        })
+        .then((response) => {
+            if(!response.ok){
+                throw new Error(response.status)
+            }
+            console.log('response:')
+            console.log(response)
+            return response.json()
+        })
+        .then((data) => {
+            saved = data
+            data.sort((a,b) => a.rank - b.rank)
+            console.log("Received data:")
+            console.log(data)
+            resolve(data)
+        })
+        .catch((error) => {
+            reject(error)
+        })
     })
-    .then((response) => {
-        if(!response.ok){
-            throw new Error('Network error')
-        }
-        return response.json()
-    })
-    .then((data) => {
-        data.sort((a,b) => a.rank - b.rank)
-        console.log("Received data:")
-        console.log(data)
-        getData = data
-    })
-    .catch(() => {
-        console.log('Network Error')
-    })
-    return ret
+        
 }
-function setBoard(boardData){
+function setBoard(boardData, reset){
     console.log("Process for: ")
     console.log(boardData)
 
-    tbody.innerHTML = ""
+    if(reset) tbody.innerHTML = ""
+
+    if(boardData.length == 0){
+        var tr = document.createElement('tr')
+        table.appendChild(tr)
+        var td = document.createElement('td')
+        tr.appendChild(td)
+        td.colSpan = 4
+        td.style.height = "200px"
+        td.textContent = "No Results"
+        return
+    }
 
     for(var i = 0; i < boardData.length; i++){
         var tr = document.createElement('tr')
@@ -83,11 +107,6 @@ function setBoard(boardData){
             var date = document.createElement('p')
             detail.appendChild(date)
             date.textContent = `Date: ${boardData[i].date}`
-
-            //detail.innerHTML = `<p>"Codes"<p>${getData[i].codes}</p></p>`
-            //detail.innerHTML += `<p>Date: ${getData[i].date}`
-            //var codes = document.querySelector("p p")
-            //codes.style.margin = "0 20px"
             tr.addEventListener('click', function () {
                 const details = this.nextElementSibling
                 details.classList.toggle('details-hide')
@@ -95,22 +114,26 @@ function setBoard(boardData){
         }        
     }
 }
-function toLeft(){
-    page--
-    if(page <= 0) page = 1
-    loadBoard()
+function loadBoard(url){
+    var apiUrl
+    if (true) apiUrl = url
+    //if url 설정에 페이지 관련 있음 => 현재 페이지 맞춰서 apiUrl 새로 만들어 전달
+    getBoard(apiUrl).then(result => {
+        //if result가 있고 url이 페이지 설정이면 페이지 값 증가/감소(url 값에 따라서), 없다면 페이지 값 그대로
+        setBoard(result, true)
+    })
+    .catch(error => {
+        console.log('Error', error)
+    })   
 }
-function toRight(){
-    page++
-    if(page > 10) page = 10
-    loadBoard()
-}
-function loadBoard(){
-    var dd = getBoard()
-    console.log(dd)
-    setTimeout(() => {
-        setBoard(getData)
-    }, 100);
+function search(){
+    var input = document.getElementById('search-box').value
+    //if input === '' && 현재 저장해둔 데이터 있음(typeof가 undefined가 아니거나 값이 null이 아니거나)
+    // => 저장해둔 값 호출, 저장해둔 건 없다면 현재 페이지 loadboard(), return
+
+    //var url = urls[] ~~~ + input
+    //loadBoard(url)
+    // 입력값을 urls[search 관련]과 합쳐서 loadBoard 호출
 }
 
-loadBoard()
+loadBoard(urls[0])
